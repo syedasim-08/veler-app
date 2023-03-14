164fc2c1c9a729ef0f3b2a0127e585ef4187b25a
syedasim-08 <syedasim@webllisto.com>
2023-03-14 12:55:50 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-import MobileWidget from './components/MobileWidget';
+// import logo from './logo.svg';
+// import './App.css';
+// import MobileWidget from './components/MobileWidget';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <MobileWidget />
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-           React Progressive Web App
-        </a>
-      </header>
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//       <MobileWidget />
+//         <img src={logo} className="App-logo" alt="logo" />
+//         {/* <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p> */}
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//            React Progressive Web App
+//         </a>
+//       </header>
      
 
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import Widget from './components/MobileWidget';
+import './App.css';
+
+
+
+const App = () => {
+  return (
+    <div className="dashboard">
+      <h1 className="page-title">Dashboard</h1>
+      <div className="widget-container">
+        <Widget size="3x2" title="Widget 1" content="Lorem ipsum dolor sit amet." />
+        <Widget size="4x2" title="Widget 2" content="Consectetur adipiscing elit." />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MobileWidget.js
+++ src/components/MobileWidget.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';
-import Draggable from 'react-draggable';
-import { ResizableBox } from 'react-resizable';
 import './Widget.css'
 
-const MobileWidget = () => {
-  const [showWidget, setShowWidget] = React.useState(true);
-  const [width, setWidth] = React.useState(400);
-  const [height, setHeight] = React.useState(400);
+const Widget = ({ size, title, content }) => {
+  const classNames = `widget widget-${size}`;
 
-  const onClose = () => {
-    setShowWidget(false);
-  };
-
-  const onResize = (event, { size }) => {
-    setWidth(size.width);
-    setHeight(size.height);
-  };
-
-  return showWidget ? (
-    <Draggable handle=".handle">
-      <ResizableBox
-        width={width}
-        height={height}
-        onResize={onResize}
-        minConstraints={[100, 100]}
-        maxConstraints={[window.innerWidth - 100, window.innerHeight - 100]}
-      >
-        <div className="widget">
-          <div className="handle">
-            <FaTimes onClick={onClose} />
-          </div>
-          <div className="content">Your widget content goes here</div>
-        </div>
-      </ResizableBox>
-    </Draggable>
-  ) : null;
+  return (
+    <div className={classNames}>
+      <h3 className="widget-title">{title}</h3>
+      <div className="widget-content">{content}</div>
+    </div>
+  );
 };
 
-export default MobileWidget;
+export default Widget;
