5e76c011372d79fd35e6c2bc947951f34c526422
syedasim-08 <syedasim@webllisto.com>
2023-03-14 12:11:57 +0530

    fixed

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <MobileWidget />
         <img src={logo} className="App-logo" alt="logo" />
         {/* <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,7 +20,7 @@ function App() {
            React Progressive Web App
         </a>
       </header>
-      <MobileWidget />
+     
 
     </div>
   );
